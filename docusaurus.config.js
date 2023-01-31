c74ef67c96a56665bbb5bbe68aeba7b0a3796fb1
Ludvig Lundgren <llundgren@cbws.nl>
2023-01-31 17:07:55 +0100

    feat: add search (#3)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,6 +32,20 @@ module.exports = {
       
     },
   },
+  themes: [
+        [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        docsRouteBasePath: "/",
+        language: "en",
+        docsDir: "docs",
+      }),
+    ],
+  ],
   presets: [
     [
       '@docusaurus/preset-classic',
